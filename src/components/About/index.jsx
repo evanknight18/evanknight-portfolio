3860928814a939e1f98d7c25e772b945e82205aa
Evan <evanknight18@gmail.com>
2023-07-18 23:21:55 -0600

    about me updated

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -7,12 +7,17 @@ function About() {
         <div className="profile-img my-5">
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
-        <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
-        </p>
-        <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
-        </p>
+        <p>Hello! I'm Evan Knight, a Denver-based, full-stack web developer and touring musician with a unique blend of creative and technical skills.</p>
+
+<p>I grew up with the steady rhythm of music shaping my life, as a drummer who has played in 16 different countries across three continents. The willpower, pattern recognition, and attention to detail I honed during my musical journey have been instrumental in my transition into web development. My background gives me a unique, unconventional perspective when it comes to coding - I like to think of each project as a symphony, with different elements coming together in harmony to create a functional, user-friendly experience.</p>
+
+<p>In 2023, I graduated from the University of Denver's Full Stack Bootcamp, where I refined my technical skills and gained expertise in HTML, CSS, JavaScript, React, Express, MongoDB, Heroku, SQL, Insomnia, GraphQL, and Handlebars. I pride myself on my focus and my ability to use these tools to craft intricate web applications. I consider myself a perpetual learner, constantly seeking out new technologies and methodologies to sharpen my craft.</p>
+
+<p>My goal is to leverage my skills and experiences to become a full-time senior full stack developer. I believe that my unique blend of creativity and technical skills, coupled with my commitment to detail and my passion for web development, make me a strong candidate for any development team.</p>
+
+<p>Fun, unconventional, and always up for a challenge, I'm excited about the future of web development and my role within it. If you're looking for a developer with an out-of-the-box perspective, a knack for pattern recognition, and a proven track record of perseverance, then I'm your man.</p>
+
+<p>Feel free to explore my portfolio and reach out if you'd like to connect!</p>
       </div>
     </section>
   );
