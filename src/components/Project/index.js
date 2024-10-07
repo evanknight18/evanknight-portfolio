b48f06f764342065f5e4720001446b819c9f128a
Evan Knight <evanknight18@gmail.com>
2024-10-07 14:21:33 -0600

    image importing fixed

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { removeHyphensAndCapitalize } from '../../utils/helpers';
 
+// Import images
+import todoListImage from '../../assets/projects/todo-list.jpg';
+import ecommerceBackendImage from '../../assets/projects/e-commerce backend.jpg';
+import runBuddyImage from '../../assets/projects/run-buddy.jpg';
+import ledWallImage from '../../assets/projects/led-wall.jpg';
+import calculatorImage from '../../assets/projects/calculator.jpg';
+
 function Project({ project }) {
 
   const { name, repo, link, description } = project;
 
+  // Map project names to the respective imported images
+  const projectImages = {
+    'todo-list': todoListImage,
+    'e-commerce backend': ecommerceBackendImage,
+    'run-buddy': runBuddyImage,
+    'led-wall': ledWallImage,
+    'calculator': calculatorImage
+  };
+
   return (
     <div className="project" key={name}>
       <img
-        src={require(`../../assets/projects/${name}.jpg`).default}
+        src={projectImages[name]} // Use mapped image here
         alt={removeHyphensAndCapitalize(name)}
         className="project-bg"
       />
